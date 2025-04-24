33eaca93b3a6b007f497f56043df9aefab59f72b
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2025-04-24 14:26:21 +0300

    update like button functionality; implement AJAX handling and allow post ID to be passed as a shortcode attribute

--- wp-content/plugins/like-button/like-button.php
+++ wp-content/plugins/like-button/like-button.php
@@ -33,16 +33,15 @@ register_activation_hook( __FILE__, 'create_table' );
 
 // Add like button
 
-function like_button($atts = [] ): string {
+function like_button( $atts ): string {
 	global $wpdb;
 
 	$table_name = $wpdb->prefix . 'likes';
 
-	$atts = shortcode_atts( [
-		'id' => 0,
-	], $atts );
-
-	$post_id = intval( $atts['id'] ) ?? get_the_ID();
+	$post_id = get_the_ID();
+	if( isset( $atts['post_id'] ) ) {
+		$post_id = $atts['post_id'];
+	}
 
 	$results = $wpdb->get_results( "SELECT * FROM $table_name WHERE post_id = $post_id" );
 
@@ -70,7 +69,7 @@ function like_button($atts = [] ): string {
 
 	$output = '<form id="like-form" method="post" action="' . admin_url( 'admin-post.php' ) . '">';
 	$output .= '<input type="hidden" name="action" value="add_like">';
-	$output .= '<input type="hidden" name="post_id" value="' . $post_id . '">';
+	$output .= '<input id="post_id" type="hidden" name="post_id" value="' . $post_id . '">';
 	$output .= '<button id="like-button" type="submit"><ion-icon name="' . $icon . '"></ion-icon></button>';
 	$output .= '<span id="like-count">' . $likes . '</span>';
 	$output .= '</form>';
@@ -119,7 +118,7 @@ add_shortcode( 'like_button', 'like_button' );
 		$success = $wpdb->insert( $table_name, $data, $format );
 
 		if ( $success ) {
-			echo 'Like added';
+			echo like_button(['post_id' => $post_id]);
 		} else {
 			echo 'Error adding like';
 		}
@@ -138,13 +137,13 @@ add_shortcode( 'like_button', 'like_button' );
 		$success = $wpdb->delete( $table_name, $where, $where_format );
 
 		if ( $success ) {
-			echo 'Data deleted';
+			echo like_button(['post_id' => $post_id]);
 		} else {
 			echo 'Error deleting data';
 		}
 	}
 
-	wp_redirect( $_SERVER['HTTP_REFERER'] ); // kommentoi pois ajax vesiossa
+	// wp_redirect( $_SERVER['HTTP_REFERER'] ); // kommentoi pois ajax vesiossa
 	exit;
 }
 
@@ -160,4 +159,16 @@ add_action( 'wp_enqueue_scripts', 'my_theme_load_ionicons_font' );
 
 // ajax toiminnallisuus
 // lisää action: add_like(), ks. single-post-ajax.php
+add_action( 'wp_ajax_add_like', 'add_like' );
+
 // enqueue skripti, ks. functions.php
+function like_button_enqueue_scripts(): void {
+	wp_register_script( 'like-button', plugin_dir_url( __FILE__ ) . '/like-button.js', [], '1.0', true );
+	$script_data = array(
+		'ajax_url' => admin_url( 'admin-ajax.php' ),
+	);
+	wp_localize_script( 'like-button', 'likeButton', $script_data );
+	wp_enqueue_script( 'like-button' );
+}
+
+add_action( 'wp_enqueue_scripts', 'like_button_enqueue_scripts' );
